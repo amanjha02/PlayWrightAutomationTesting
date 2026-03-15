1790e3d38e4f1694745c3924be401bfbd055029e
AmanJha <aman02bitm@gmail.com>
2026-03-15 14:14:24 +0530

    All fiel updated

---
+++ Chapter_08_Arrays/ArrayCreation.js
@@ -0,0 +1,16 @@
+let fruits=[] //empty array
+let fruit_fresh=["apple","banana","grapes"] //array with 3 elements
+let numbers=[1,2,3,4,5] //array with 5 elements
+let mixed_array=[1,"hello",true,null] //array with different types of elements
+console.log(fruits) // []
+console.log(fruit_fresh[0]) // ["apple","banana","grapes"]
+console.log(numbers) // [1,2,3,4,5]
+console.log(mixed_array) // [1,"hello",true,null]
+console.log(fruit_fresh.length);
+console.log();
+console.log();
+console.log("------------------------------------")
+let number=[1,2,3,4,5,6];
+console.log(number[1]);
+console.log(number)
+
---
+++ Chapter_08_Arrays/important link.js
@@ -0,0 +1,5 @@
+https://app.eraser.io/workspace/Tz63v1Pz52oXHd70LdyM --- notes
+
+https://courses.thetestingacademy.com/courses/playwright-automation-mastery/contents/696f7fab8c157 ---- recording 
+
+https://github.com/PramodDutta/LearningPlaywrightBatch  ------repo 
